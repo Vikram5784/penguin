e05e38c30cf641f483e84010c06d06d20f8bff94
Vikram5784 <141595239+Vikram5784@users.noreply.github.com>
2023-08-19 18:59:39 -0500

    Add files via upload

---
+++ penguin_app.py
@@ -0,0 +1,10 @@
+@st.cache()
+def prediction(model, island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex):
+  species = model.predict([[island, bill_length_mm, bill_depth_mm, flipper_length_mm, body_mass_g, sex]])
+  species = species[0]
+  if species == 0:
+    return "Adelie"
+  elif species == 1:
+    return "Chinstrap"
+  else:
+    return "Gentoo"
